cdec489aaf8dd59029e880717127f0ab895a767e
Nicola Zilio <nicolazilio@gmail.com>
2023-07-30 22:00:47 +0200

    Temp initial

--- parkour_app/library_sample_shared/admin.py
+++ parkour_app/library_sample_shared/admin.py
@@ -129,14 +129,70 @@ class IndexTypeAdmin(admin.ModelAdmin):
         return super().change_view(request, object_id, form_url, extra_context)
 
 
+from import_export import fields, resources
+# from import_export.admin import ImportMixin
+
+from import_export.widgets import ForeignKeyWidget
+
+class IndexPairImportResource(resources.ModelResource):
+
+    index_type = fields.Field(
+        column_name='index_type_name',
+        attribute='name',
+        widget=ForeignKeyWidget(IndexType, field='name'))
+
+    index_i7_prefix = fields.Field(
+        column_name='index_i7_prefix',
+        attribute='prefix',
+        widget=ForeignKeyWidget(IndexI7, field='prefix'))
+    
+    index_i7_number = fields.Field(
+        column_name='index_i7_number',
+        attribute='number',
+        widget=ForeignKeyWidget(IndexI7, field='number'))
+
+    index_i7_index = fields.Field(
+        column_name='index_i7_index',
+        attribute='index',
+        widget=ForeignKeyWidget(IndexI7, field='index'))
+
+    index_i5_prefix = fields.Field(
+        column_name='index_i5_prefix',
+        attribute='prefix',
+        widget=ForeignKeyWidget(IndexI5, field='prefix'))
+
+    index_i5_number = fields.Field(
+        column_name='index_i5_number',
+        attribute='number',
+        widget=ForeignKeyWidget(IndexI5, field='number'))
+    
+    index_i5_index = fields.Field(
+        column_name='index_i5_index',
+        attribute='index',
+        widget=ForeignKeyWidget(IndexI5, field='index'))
+
+    class Meta:
+        model = IndexPair
+        fields = ('index_type', 'index_i7_prefix', 'index_i7_number',
+                  'index_i7_index', 'index_i5_prefix', 'index_i5_number',
+                  'index_i5_index', 'char_coord', 'num_coord')
+        import_id_fields = ('index_type',)
+
+
+    def before_import_row(self, row, **kwargs):
+        logger.error(row)
+
+import logging
+logger = logging.Logger('django')
 @admin.register(IndexPair)
-class IndexPairAdmin(admin.ModelAdmin):
+class IndexPairAdmin(ImportExportModelAdmin):
     list_display = (
         "index_pair",
         "coordinate",
     )
     search_fields = ("index_type__name",)
     list_filter = ("index_type",)
+    # resource_classes = [IndexPairImportResource]
 
     def index_pair(self, obj):
         return str(obj)
